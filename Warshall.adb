5e1f55bd853ba39839026e8c8867401564edc19d
Jaken Herman <jch053@shsu.edu>
2016-09-16 10:48:47 -0500

    Update Warshall.adb

--- Warshall.adb
+++ Warshall.adb
@@ -4,38 +4,84 @@ procedure Warshall is
    package Iio is new Ada.Text_Io.Integer_Io(Integer);
    use Iio;
 
-   --Integer Array needs conversion to generic type
-   M : array (1 .. 8, 1 .. 8) of Integer := ((0, 0, 0, 0, 0, 0, 0, 0),(0, 0, 0, 0, 0, 0, 0, 0),(0, 0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0, 0), (0, 0, 0, 0, 0, 0, 0, 0));
+   --Get_Size allows the txt file to specify how much space to allocate.
+   function Get_Size return Integer  is
+      Filename : String := "win.txt";
+      File : Ada.Text_IO.File_Type;
+      Line_Count : Integer := 0;
+      ReturnSize : Integer;
+   begin
+      Ada.Text_IO.Open(File => File,
+                       Mode => Ada.Text_IO.In_File,
+                       Name => Filename);
+      while Line_Count /= 1 loop
+         declare
+            Line : String := Ada.Text_IO.Get_Line (File);
+         begin
+            ReturnSize := Integer'Value(Line);
+            Line_Count := 1;
+         end;
+      end loop;
+      Ada.Text_IO.Close (File);
+      return ReturnSize;
+   end Get_Size;
 
+   --Create variables  to set up file i/o
+   Filename : String := "win.txt";
+   File : Ada.Text_IO.File_Type;
+
+   --Control x/y on file read
+   X_Or_Y : Integer := 99;
+
+   --Create variables for X and Y to be set by user.
+   User_X : Integer;
+   User_Y : Integer;
+   User_Size : Integer := Get_Size;
+   
+   --Integer Array needs conversion to generic type
+   M : array (1 .. User_Size, 1 .. User_Size) of Integer := (others => (others => 0));
 
    -- "First" allows the user to create edges between
    -- vertices.
    procedure First (
-         X : in     Integer;
-         Y : in     Integer) is
+                    X : in     Integer;
+                    Y : in     Integer) is
    begin
       --This coordinate needs to be "1" because the node exists
       M(X, Y) := 1;
    end;
 
-   --Create variables for X and Y to be set by user.
-   User_X : Integer;
-   User_Y : Integer;
-
-   --begin "Warshall".
+--begin "Warshall".
 begin
+   Ada.Text_IO.Open (File => File,
+                     Mode => Ada.Text_IO.In_File,
+                     Name => Filename);
 
-   --Receive user input values.
-   for I in 1..8 loop 
-      Get(User_X); Get(User_Y);
-      First(User_X, User_Y);
-   end loop;    
- 
+
+   while not Ada.Text_IO.End_Of_File (File) loop
+      declare
+         Line : String := Ada.Text_IO.Get_Line (File);
+      begin
+         if X_Or_Y = 99 then
+            User_Size := Integer'Value(Line);
+            X_Or_Y := 0;
+         elsif X_Or_Y = 0 then
+            User_X := Integer'Value(Line);
+            X_Or_Y := 1;
+         else
+            User_Y := Integer'Value(Line);
+            First(User_X, User_Y);
+            X_Or_Y := 0;
+         end if;
+      end;
+   end loop;
+   Ada.Text_IO.Close (File);
+
    --Warshall's algorithm
-   for I in 1..8 loop
-      for J in 1..8 loop
+   for I in 1..User_Size loop
+      for J in 1..User_Size loop
          if M(J,I) > 0 then
-            for K in 1..8 loop
+            for K in 1..User_Size loop
                M(J,K) := M(J,K) + M(I,K);
             end loop;
          end if;
@@ -43,9 +89,9 @@ begin
    end loop;
 
    --print the adjacency matrix.
-   for I in 1..8 loop
+   for I in 1..User_Size loop
       New_Line;
-      for J in 1..8 loop
+      for J in 1..User_Size loop
          if M(I,J) > 0 then
             Put("1 ");
          else
@@ -53,5 +99,4 @@ begin
          end if;
       end loop;
    end loop;
-
-end;	    
+end;
