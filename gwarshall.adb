d0b78bca4f873817c9569687e82bb0f33903740f
Jaken Herman <jch053@shsu.edu>
2016-09-28 01:18:16 -0500

    Update gwarshall.adb

--- gwarshall.adb
+++ gwarshall.adb
@@ -1,14 +1,23 @@
-with Ada.Text_Io; use Ada.Text_Io;
-with gwar; 
+with gwar;
+with Ada.Text_Io;
 procedure gwarshall is
 
-package iio is new Ada.Text_Io.Integer_IO(Integer); use iio;
-package ngwar is new gwar(integer, 100);
-use ngwar;
+    package iio is new Ada.Text_IO.Integer_IO(Integer);
+    use iio;
 
-x : integer;
+  package MyFloatIO is new Ada.Text_IO.Float_IO(Float);
+  use MyFloatIO;
 
+ procedure FloatPut(X: Float) is
+  begin    MyFloatIO.Put(X, 0, 0, 0);  end;
+
+procedure StrPut(X: String) is 
+  begin    Ada.Text_IO.Put(X); end;
+
+  procedure IntPut(X: Integer) is
+  begin    iio.Put(X); end;
+
+    package ch_gwar is new gwar(Integer, IntPut);	use ch_gwar;
 begin
-    printsize(x);
-    put(x);
+    null;
 end gwarshall;
