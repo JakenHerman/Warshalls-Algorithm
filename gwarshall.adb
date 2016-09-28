4e098415fbe173a137e354a48a4d5ede3789ff80
Jaken Herman <jch053@shsu.edu>
2016-09-28 14:14:47 -0500

    Update gwarshall.adb

--- gwarshall.adb
+++ gwarshall.adb
@@ -2,22 +2,22 @@ with gwar;
 with Ada.Text_Io;
 procedure gwarshall is
 
+    subtype s3 is string (1..3);
+    subtype Character is string (1 .. 1);
     package iio is new Ada.Text_IO.Integer_IO(Integer);
     use iio;
-
-  package MyFloatIO is new Ada.Text_IO.Float_IO(Float);
-  use MyFloatIO;
-
- procedure FloatPut(X: Float) is
-  begin    MyFloatIO.Put(X, 0, 0, 0);  end;
-
-procedure StrPut(X: String) is 
+ 
+procedure ChrPut(X : Character) is begin Ada.Text_Io.Put(x); end;
+procedure StrPut(X: s3) is 
   begin    Ada.Text_IO.Put(X); end;
 
   procedure IntPut(X: Integer) is
   begin    iio.Put(X); end;
 
-    package ch_gwar is new gwar(Integer, IntPut);	use ch_gwar;
+    package d_option_data is new gwar(Character, "d_Option.txt",  ChrPut);	use d_option_data;
+    package c_option_data is new gwar(Character, "c_Option.txt", ChrPut); use c_option_data;
+    package b_option_data is new gwar(s3, "b_Option.txt", StrPut); use b_option_data;
+    package a_option_data is new gwar(Integer, "a_Option.txt", IntPut); use a_option_data;
 begin
-    null;
+    declare
 end gwarshall;
